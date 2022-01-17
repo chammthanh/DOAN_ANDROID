5f2513dfeab9f213c82f0d14dee2390977ac661a
chammthanh <nguyenthithanhtram311@gmail.com>
2022-01-17 20:47:00 +0700

    edit project

--- lib/screens/donmua/dahuy/components/hinhanh.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget hinhanh = SizedBox(
-  width: 90,
-  height: 90,
-  child: Image.asset(
-    "assets/images/products/product-01.jpg",
-  ),
-);
--- lib/widgets/line.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget line = Container(
-  margin: const EdgeInsets.only(top: 12, bottom: 12),
-  height: 0.5,
-  width: double.maxFinite,
-  color: Colors.grey.shade300,
-);
