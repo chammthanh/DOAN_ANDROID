66f296c6665303c976d4269133705a1baa9eaa9a
ngotuanchau <0306191299@caothang.edu.vn>
2022-02-10 14:10:54 +0700

    api load sanpham

--- api/app/Http/Controllers/SanPhamController.php
+++ api/app/Http/Controllers/SanPhamController.php
@@ -7,7 +7,21 @@ use Illuminate\Http\Request;
 
 class SanPhamController extends Controller
 {
-    function test()
+    function sanpham()
     {
+        $danhSach =
+            sanpham::select('*')
+            ->join('chitietsanpham', 'sanpham.id', '=', 'chitietsanpham.sanpham_id')
+            ->get();
+        if (empty($danhSach)) {
+            return json_encode([
+                'success' => false,
+                'message' => 'Lỗi load dữ liệu'
+            ]);
+        }
+        return json_encode([
+            'success' => true,
+            'data' => $danhSach
+        ]);
     }
 }
